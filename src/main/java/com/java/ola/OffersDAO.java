8e80956f67c37ec35b4b495ee33be2060d1c5fd8
Ola Karlsson <ola.karlsson@bahnhof.se>
2017-08-19 14:14:35 +0200

    Added transactional

--- src/main/java/com/java/ola/OffersDAO.java
+++ src/main/java/com/java/ola/OffersDAO.java
@@ -14,6 +14,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component("offersDao")
 public class OffersDAO {
@@ -34,7 +35,7 @@ public class OffersDAO {
 	}
 	
 	//Batch job
-	
+	@Transactional
 	public int [] create(List<Offer> offer){
 		
 		SqlParameterSource[] params = SqlParameterSourceUtils.createBatch(offer.toArray());
